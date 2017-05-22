de55518f4e5bf102105001f27ff446a115ad4157
Gyorgy Orosz <oroszgy@gmail.com>
2017-05-22 09:28:22 +0200

    Python3 compatibility

--- join_forum_data/join_forum_data.py
+++ join_forum_data/join_forum_data.py
@@ -13,9 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import praw
+from __future__ import print_function
+
 import json
 import time
+
+import praw
 
 '''
 Requirements: PRAW python library - https://praw.readthedocs.io/en/latest/index.html
@@ -47,7 +50,6 @@ https://bigquery.cloud.google.com/dataset/fh-bigquery:reddit_posts
 and using that data instead of the Reddit API to collect the comment texts and author names.
 '''
 
-
 # Replace below with information provided to you by Reddit when registering your script
 reddit = praw.Reddit(client_id='my client id',
                      client_secret='my client secret',
@@ -56,33 +58,33 @@ reddit = praw.Reddit(client_id='my client id',
 with open('coarse_discourse_dataset.json') as jsonfile:
     lines = jsonfile.readlines()
     dump_with_reddit = open('coarse_discourse_dump_reddit.json', 'w')
-    
+
     for line in lines:
         reader = json.loads(line)
-        print reader['url']
-        
+        print(reader['url'])
+
         submission = reddit.submission(url=reader['url'])
-        
+
         # Annotators only annotated the 40 "best" comments determined by Reddit
         submission.comment_sort = 'best'
         submission.comment_limit = 40
-        
+
         post_id_dict = {}
-        
+
         for post in reader['posts']:
             post_id_dict[post['id']] = post
-        
+
         try:
             full_submission_id = 't3_' + submission.id
             if full_submission_id in post_id_dict:
                 post_id_dict[full_submission_id]['body'] = submission.selftext
-                
+
                 # For a self-post, this URL will be the same URL as the thread.
                 # For a link-post, this URL will be the link that the link-post is linking to.
                 post_id_dict[full_submission_id]['url'] = submission.url
                 if submission.author:
                     post_id_dict[full_submission_id]['author'] = submission.author.name
-            
+
             submission.comments.replace_more(limit=0)
             for comment in submission.comments.list():
                 full_comment_id = 't1_' + comment.id
@@ -92,19 +94,18 @@ with open('coarse_discourse_dataset.json') as jsonfile:
                         post_id_dict[full_comment_id]['author'] = comment.author.name
 
         except Exception as e:
-            print 'Error %s' % (e)
-        
+            print('Error %s' % (e))
+
         found_count = 0
         for post in reader['posts']:
             if not post.has_key('body'):
-                print "Can't find %s in URL: %s" % (post['id'], reader['url'])
+                print("Can't find %s in URL: %s" % (post['id'], reader['url']))
             else:
                 found_count += 1
-                
-        print 'Found %s posts out of %s' % (found_count, len(reader['posts']))
+
+        print('Found %s posts out of %s' % (found_count, len(reader['posts'])))
 
         dump_with_reddit.write(json.dumps(reader) + '\n')
-        
+
         # To keep within Reddit API limits
         time.sleep(2)
-        
